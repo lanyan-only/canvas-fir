9c2b76c22c5a38080e343d7cb69b765eb4077593
lanyan-only <894468157@qq.com>
2016-02-19 09:59:53 +0800

    用图片做棋子

--- js/index.js
+++ js/index.js
@@ -1,54 +1,59 @@
 window.onload=function(){
 	var canvas=document.querySelector("#canvas");
 	var ctx=canvas.getContext("2d");
-	var canvas00=document.querySelector("#canvas00");
-	var ctx00=canvas00.getContext("2d");
-	
-    ctx00.strokeStyle="#030a56";
-    var row=15;
-	var y0=20.5;var x0=20.5;
-    for(i=0;i<row;i++){
-        //heng
-        var heng=ctx00.createLinearGradient(0,0,600,0);
-        heng.addColorStop(0,'red');
-        heng.addColorStop(0.5,'green');
-        ctx00.strokeStyle=heng;
-    	ctx00.beginPath();
-    	ctx00.moveTo(20,y0);
-    	ctx00.lineTo(580,y0);
-    	y0+=40;
-        ctx00.stroke();
 
-	    //shu
-	    var heng=ctx00.createLinearGradient(0,0,0,600);
-        heng.addColorStop(0,'yellow');
-        heng.addColorStop(0.5,'blue');
-        ctx00.strokeStyle=heng;
-    	ctx00.beginPath();
-		ctx00.moveTo(x0,20);
-		ctx00.lineTo(x0,580);
-		x0+=40;
-		ctx00.stroke();
+
+    var row=15;//棋盘大小
+    var qizi={};//所有的落子数据
+    var kaiguan=localStorage.x?false:true;
+    var huaqipan=function(){
+	    	ctx.strokeStyle="#030a56";
+	    	var y0=20.5;var x0=20.5;
+	    	for(i=0;i<row;i++){
+	        //heng
+	        var heng=ctx.createLinearGradient(0,0,600,0);
+	        heng.addColorStop(0,'red');
+	        heng.addColorStop(0.5,'green');
+	        ctx.strokeStyle=heng;
+	        ctx.beginPath();
+	        ctx.moveTo(20,y0);
+	        ctx.lineTo(580,y0);
+	        y0+=40;
+	        ctx.stroke();
+
+		    //shu
+		    var heng=ctx.createLinearGradient(0,0,0,600);
+		    heng.addColorStop(0,'yellow');
+		    heng.addColorStop(0.5,'blue');
+		    ctx.strokeStyle=heng;
+		    ctx.beginPath();
+		    ctx.moveTo(x0,20);
+		    ctx.lineTo(x0,580);
+		    x0+=40;
+		    ctx.stroke();
+		    }
+
+		    //固定小黑点
+		    ctx.beginPath();
+		    ctx.arc(300.5,300.5,5,0,Math.PI*2);
+		    ctx.fill();
+
+			var z0=[140.5,460.5];//棋盘星点位置数据
+			for (var i = 0; i < z0.length; i++) {
+				for (var j = 0; j < z0.length; j++) {
+					var heng=ctx.createLinearGradient(300,300,305,305);
+					heng.addColorStop(0,'blue');
+					heng.addColorStop(0.3,'yellowgreen');
+					ctx.fillStyle=heng;
+					ctx.beginPath();
+					ctx.arc(z0[i],z0[j],5,0,Math.PI*2);
+					ctx.fill();
+				}
+				
+			}
+
     }
-	
-    //固定小黑点
-	ctx00.beginPath();
-	ctx00.arc(300.5,300.5,5,0,Math.PI*2);
-	ctx00.fill();
-
-	var z0=[140.5,460.5];
-	for (var i = 0; i < z0.length; i++) {
-		for (var j = 0; j < z0.length; j++) {
-			    var heng=ctx00.createLinearGradient(300,300,305,305);
-			    heng.addColorStop(0,'blue');
-			    heng.addColorStop(0.3,'yellowgreen');
-			    ctx00.fillStyle=heng;
-			    ctx00.beginPath();
-				ctx00.arc(z0[i],z0[j],5,0,Math.PI*2);
-				ctx00.fill();
-		}
-		
-	}
+    huaqipan();
 
 // 渐变线条
 /*var lingrad=ctx.createLinearGradient(20,300,580,300);
@@ -70,50 +75,67 @@ ctx.stroke();
 ctx.fillStyle=lingrad;
 ctx.fillRect(0,0,600,200);*/
 
+/*
+  x  num  x轴
+  y  num  y轴
+  */
+
+  var luozi2=function(x,y,color){
+  	var zx=40*x+20.5;
+  	var zy=40*y+20.5;
+  	var black=ctx.createRadialGradient(zx,zy,1,zx,zy,18);
+  	black.addColorStop(0.1,'#444');
+  	black.addColorStop(1,'black');
 
 
-var luozi=function(x,y,color){
-	var zx=40*x+20.5;
-	var zy=40*y+20.5;
-	var black=ctx.createRadialGradient(zx,zy,1,zx,zy,18);
-	   black.addColorStop(0.1,'#444');
-	   black.addColorStop(1,'black');
-      
-
-	var white=ctx.createRadialGradient(zx,zy,1,zx,zy,18);
-	white.addColorStop(0.1,'#fff');
-	white.addColorStop(1,'#ddd');
+  	var white=ctx.createRadialGradient(zx,zy,1,zx,zy,18);
+  	white.addColorStop(0.1,'#fff');
+  	white.addColorStop(1,'#ddd');
 
 
-    /*ctx.fillStyle=color?black:white;*/
-     if(color==true){
-         ctx.fillStyle=black;
-     }else{
-     	ctx.fillStyle=white;
-     }
-	ctx.beginPath();
-	ctx.arc(zx,zy,18,0,Math.PI*2);
-	ctx.fill();
+  	/*ctx.fillStyle=color?black:white;*/
+  	if(color==true){
+  		ctx.fillStyle=black;
+  	}else{
+  		ctx.fillStyle=white;
+  	}
+  	ctx.beginPath();
+  	ctx.arc(zx,zy,18,0,Math.PI*2);
+  	ctx.fill();
 
-}
+  }
 /*luozi(3,3,true);
 luozi(4,3,false);
 luozi(7,7,true);*/
+var qiziimg=document.querySelector("#sucai");
+var qiziimghei=document.querySelector("#sucaihei");
 
+var luozi=function(x,y,color){
+	var zx=40*x+5.5;
+	var zy=40*y+5.5;
+	if(color){
+		ctx.drawImage(qiziimg,zx,zy,36,36);
+	}else{
+		ctx.drawImage(qiziimghei,zx,zy,36,36);
+	}
+	
+}
 
-var qizi={}
-var kaiguan=true;
 canvas.onclick=function(e){
-
 	var x=Math.round((e.offsetX-20.5)/40);
 	var y=Math.round((e.offsetY-20.5)/40)
-    if(qizi[x+'-'+y]){return};
+	if(qizi[x+'-'+y]){return};
 
 	luozi(x,y,kaiguan);
-	 qizi[x+'-'+y]=kaiguan?'black':'white';
+	qizi[x+'-'+y]=kaiguan?'black':'white';
 	kaiguan=!kaiguan;
 
-   localStorage.data=JSON.stringify(qizi);
+	localStorage.data=JSON.stringify(qizi);
+	if(!kaiguan){
+		localStorage.x=1;
+	}else{
+		localStorage.removeItem("x");
+	}
 }
 
 
@@ -134,7 +156,7 @@ canvas.onclick=function(e){
    localStorage.data=JSON.stringify(qizi);
    
 }*/
-
+/*如果本地存储中有棋盘数据，读取这些数据并绘制到页面中*/
 
 if(localStorage.data){
 	qizi=JSON.parse(localStorage.data);
@@ -154,7 +176,7 @@ document.ondblclick=function(){
 }
 button.onclick=function(){
 	localStorage.clear();
-	
+	location.reload();
 }
 
 
